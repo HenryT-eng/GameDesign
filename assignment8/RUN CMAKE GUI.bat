850fa5078a9204520cc19e075c751c68b253b14e
Henry Tetlak <60018651+HenryT-eng@users.noreply.github.com>
2024-07-29 17:27:33 -0400

    Delete assignment8 directory

--- assignment8/RUN CMAKE GUI.bat
+++
@@ -1,2 +0,0 @@
-cmake -G "Visual Studio 17 2022" -A x64 -S ./src/ -B ./cwin64
-start cmake-gui -G "Visual Studio 17 2022" -A x64 -S ./src/ -B ./cwin64
--- assignment8/RUN CMAKE.bat
+++
@@ -1,3 +0,0 @@
-cmake -G "Visual Studio 17 2022" -A x64 -S ./src/ -B ./cwin64
-pause
-
